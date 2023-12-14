582111189bb810cf3da48575f4cb8d52df68105d
Hiram Fernandes <hiram.sd@gmail.com>
2023-12-14 11:06:31 -0300

    Added Vendor GetById Method on Controller

--- aspnet-mongo/Controllers/VendorsController.cs
+++ aspnet-mongo/Controllers/VendorsController.cs
@@ -1,4 +1,5 @@
-﻿using aspnet_mongo.Models.DTO;
+﻿using aspnet_mongo.Models;
+using aspnet_mongo.Models.DTO;
 using aspnet_mongo.Services;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -18,12 +19,21 @@ namespace aspnet_mongo.Controllers
         }
 
         [HttpGet]
+        [Produces(typeof(IEnumerable<Vendor>))]
         public async Task<IActionResult> GetVendors(CancellationToken cancellationToken)
         {
             // TODO: Create output DTO
             var vendors = await _vendorService.GetAllAsync();
-
             return Ok(vendors);
+        }
+
+        [HttpGet("{id}")]
+        [Produces(typeof(Vendor))]
+        public async Task<IActionResult> GetVendorById(string id)
+        {
+            var vendor = await _vendorService.GetById(id);
+
+            return Ok(vendor);
         }
 
         [HttpPost]
