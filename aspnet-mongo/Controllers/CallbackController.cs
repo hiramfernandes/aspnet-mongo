7e287f1b54cd2390855f637663327bfe8e7a5a6f
Hiram Fernandes <hiram.sd@gmail.com>
2026-04-01 15:21:00 -0300

    small refactoring before extraction

--- aspnet-mongo/Controllers/CallbackController.cs
+++ aspnet-mongo/Controllers/CallbackController.cs
@@ -42,108 +42,35 @@ namespace aspnet_mongo.Controllers
             if (update.Message is not { } message)
                 return BadRequest($"Error when processing telegram update (null object)");
 
-            // Callback - used to obtain a Telegram payload for further testing (locally)
-
             if (_openAiSettings.TestMode)
             {
                 var jsonMessage = JsonSerializer.Serialize(message);
                 await _telegramBotClient.SendMessage(message!.Chat.Id, $"Telegram message: {jsonMessage} ");
             }
 
-            // Url Based info
-            if (message.Text != null)
+            try
             {
-                var url = message.Text;
-                var httpClient = _httpClientFactory.CreateClient("Scraper");
-                try
+                // Url Based info
+                if (message.Text != null)
                 {
-                    using var response = await httpClient.GetAsync(url, HttpCompletionOption.ResponseContentRead, cancellationToken);
-
-                    response.EnsureSuccessStatusCode();
-
-                    // Return the full HTML string
-                    var htmlContent = await response.Content.ReadAsStringAsync(cancellationToken);
-
-                    var systemPrompt = System.IO.File.ReadAllText("Prompts/ExtractReceiptBasedOnHtmlContent.txt");
-
-                    var userMessage = $"""  
-                        HTML: {htmlContent}  
-                        URL: {url}  
-                        """;
-
-                    var llmResponse = await SendInfoToLlmAsync(systemPrompt, userMessage);
-
-                    if (_openAiSettings.TestMode)
-                    {
-                        using var jsonStream = new MemoryStream(System.Text.Encoding.UTF8.GetBytes(llmResponse));
-                        await _telegramBotClient.SendDocument(
-                            message!.Chat.Id,
-                            InputFile.FromStream(jsonStream, "receipt_parsed.json"));
-                    }
-
-                    var nfcReceipt = JsonSerializer.Deserialize<NfcReceipt>(llmResponse);
-
-                    await SavePurchaseAsync(nfcReceipt!, message.Chat.Id, url);
+                    var url = message.Text;
+                    await HandleReceiptUrl(url, message!.Chat.Id, cancellationToken);
                 }
-                catch (HttpRequestException ex)
+
+                if (message?.Photo != null)
                 {
-                    return BadRequest(ex.Message);
+                    await _telegramBotClient.SendMessage(message!.Chat.Id, "Image received! Processing...");
+
+                    // Getting file info and downloading it
+                    var fileId = message.Photo?.Last().FileId ?? message.Document?.FileId ?? throw new Exception("File not found");
+                    await HandleQrCode(fileId, message!.Chat.Id, cancellationToken);
+
+                    // await HandleImage(fileId, message!.Chat.Id, cancellationToken);
                 }
             }
-
-            if (message?.Photo != null)
+            catch (Exception ex)
             {
-                await _telegramBotClient.SendMessage(message!.Chat.Id, "Image received! Processing...");
-
-                try
-                {
-                    // Getting file info and downloading it
-                    var fileId = message.Photo?.Last().FileId ?? message.Document?.FileId;
-                    var fileInfo = await _telegramBotClient.GetFile(fileId!);
-
-                    using var stream = new MemoryStream();
-                    await _telegramBotClient.DownloadFile(fileInfo.FilePath!, stream, cancellationToken);
-
-                    stream.Position = 0;
-
-                    var imageBinary = BinaryData.FromStream(stream);
-
-                    // Sending info to LLM - QR Code reader
-                    var qrCodeReaderPromt = await System.IO.File.ReadAllTextAsync("Prompts/ExtractQrCodeBasedOnImage.txt");
-                    var qrDecodingOutput = await SendInfoToLlmAsync(qrCodeReaderPromt, imageBinary);
-
-                    await _telegramBotClient.SendMessage(
-                        message!.Chat.Id,
-                        $"QR Image Analyzer obtained the following info: {qrDecodingOutput}"
-                    );
-
-                    return Ok();
-
-                    // Sending info to LLM - Receipt
-                    var promptMessage = System.IO.File.ReadAllText("Prompts/ExtractReceiptBasedOnImage.txt");
-                    var modelAnalysisOutput = await SendInfoToLlmAsync(promptMessage, imageBinary);
-
-                    if (_openAiSettings.TestMode)
-                    {
-                        using var jsonStream = new MemoryStream(System.Text.Encoding.UTF8.GetBytes(modelAnalysisOutput));
-                        await _telegramBotClient.SendDocument(
-                            message!.Chat.Id,
-                            InputFile.FromStream(jsonStream, "receipt_parsed.json"));
-                    }
-
-                    var jsonOptions = new JsonSerializerOptions()
-                    {
-                        PropertyNameCaseInsensitive = true,
-                    };
-
-                    var obtainedReceiptData = JsonSerializer.Deserialize<NfcReceipt>(modelAnalysisOutput, jsonOptions);
-
-                    await SavePurchaseAsync(obtainedReceiptData!, message.Chat.Id);
-                }
-                catch (Exception ex)
-                {
-                    await _telegramBotClient.SendMessage(message!.Chat.Id, $"An error occurred: {ex.Message}");
-                }
+                await _telegramBotClient.SendMessage(message!.Chat.Id, $"An error occurred: {ex.Message}");
             }
 
             return Ok();
@@ -187,6 +114,91 @@ namespace aspnet_mongo.Controllers
             var modelAnalysisOutput = completion.Value.Content[0].Text;
 
             return modelAnalysisOutput;
+        }
+
+        private async Task HandleReceiptUrl(string url, long messageId, CancellationToken cancellationToken)
+        {
+            var httpClient = _httpClientFactory.CreateClient("Scraper");
+            using var response = await httpClient.GetAsync(url, HttpCompletionOption.ResponseContentRead, cancellationToken);
+
+            response.EnsureSuccessStatusCode();
+
+            // Return the full HTML string
+            var htmlContent = await response.Content.ReadAsStringAsync(cancellationToken);
+
+            var systemPrompt = System.IO.File.ReadAllText("Prompts/ExtractReceiptBasedOnHtmlContent.txt");
+
+            var userMessage = $"""  
+                        HTML: {htmlContent}  
+                        URL: {url}  
+                        """;
+
+            var llmResponse = await SendInfoToLlmAsync(systemPrompt, userMessage);
+
+            if (_openAiSettings.TestMode)
+            {
+                using var jsonStream = new MemoryStream(System.Text.Encoding.UTF8.GetBytes(llmResponse));
+                await _telegramBotClient.SendDocument(
+                    messageId,
+                    InputFile.FromStream(jsonStream, "receipt_parsed.json"));
+            }
+
+            var nfcReceipt = JsonSerializer.Deserialize<NfcReceipt>(llmResponse);
+
+            await SavePurchaseAsync(nfcReceipt!, messageId, url);
+        }
+
+        private async Task HandleQrCode(string fileId, long chatMessageId, CancellationToken cancellationToken)
+        {
+            await _telegramBotClient.SendMessage(chatMessageId, "Image received! Processing...");
+            var imageBinary = await GetImageBinaryData(fileId, cancellationToken);
+
+            // Sending info to LLM - QR Code reader
+            var qrCodeReaderPromt = await System.IO.File.ReadAllTextAsync("Prompts/ExtractQrCodeBasedOnImage.txt");
+            var qrDecodingOutput = await SendInfoToLlmAsync(qrCodeReaderPromt, imageBinary);
+
+            await _telegramBotClient.SendMessage(
+                chatMessageId,
+                $"QR Image Analyzer obtained the following info: {qrDecodingOutput}"
+            );
+        }
+
+        private async Task<BinaryData> GetImageBinaryData(string fileId, CancellationToken cancellationToken)
+        {
+            var fileInfo = await _telegramBotClient.GetFile(fileId!);
+
+            using var stream = new MemoryStream();
+            await _telegramBotClient.DownloadFile(fileInfo.FilePath!, stream, cancellationToken);
+
+            stream.Position = 0;
+
+            return BinaryData.FromStream(stream);
+        }
+
+        private async Task HandleImage(string fileId, long chatMessageId, CancellationToken cancellationToken)
+        {
+            // Sending info to LLM - Receipt
+            var imageBinary = await GetImageBinaryData(fileId, cancellationToken);
+
+            var promptMessage = System.IO.File.ReadAllText("Prompts/ExtractReceiptBasedOnImage.txt");
+            var modelAnalysisOutput = await SendInfoToLlmAsync(promptMessage, imageBinary);
+
+            if (_openAiSettings.TestMode)
+            {
+                using var jsonStream = new MemoryStream(System.Text.Encoding.UTF8.GetBytes(modelAnalysisOutput));
+                await _telegramBotClient.SendDocument(
+                    chatMessageId,
+                    InputFile.FromStream(jsonStream, "receipt_parsed.json"));
+            }
+
+            var jsonOptions = new JsonSerializerOptions()
+            {
+                PropertyNameCaseInsensitive = true,
+            };
+
+            var obtainedReceiptData = JsonSerializer.Deserialize<NfcReceipt>(modelAnalysisOutput, jsonOptions);
+
+            await SavePurchaseAsync(obtainedReceiptData!, chatMessageId);
         }
 
         private async Task<string> SendInfoToLlmAsync(
