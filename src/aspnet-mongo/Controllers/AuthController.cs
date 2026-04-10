59fd10589b8ce6fb532238df781f2c1eb7202a18
Hiram Fernandes <hiram.sd@gmail.com>
2026-04-10 19:17:29 -0300

    Added AuthController Reduced Swagger exposure to lower envs

---
+++ src/aspnet-mongo/Controllers/AuthController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace aspnet_mongo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        [HttpPost("login")]
+        public void Login([FromBody] string value)
+        {
+
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetUserInfoAsync()
+        {
+            await Task.Delay(500);
+
+            return Ok();
+        }
+    }
+}
--- src/aspnet-mongo/Program.cs
+++ src/aspnet-mongo/Program.cs
@@ -6,84 +6,86 @@ using Purchases.Domain.Contracts.Services;
 using Purchases.Domain.Models.Settings;
 using Purchases.Infrastructure.Repository;
 
-namespace aspnet_mongo
+namespace aspnet_mongo;
+
+public class Program
 {
-    public class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
+        var allowReactAppCorsName = "AllowReactApp";
+
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add CORS
+        builder.Services.AddCors(options =>
         {
-            var allowReactAppCorsName = "AllowReactApp";
-
-            var builder = WebApplication.CreateBuilder(args);
-
-            // Add CORS
-            builder.Services.AddCors(options =>
+            options.AddPolicy(name: allowReactAppCorsName,
+            policy =>
             {
-                options.AddPolicy(name: allowReactAppCorsName,
-                policy =>
-                {
-                    policy
-                        .WithOrigins(
-                            "https://react-purchases.vercel.app"
-                            //"http://localhost:5173" // In case testing FE locally
-                            )
-                        .AllowCredentials();
+                policy
+                    .WithOrigins(
+                        "https://react-purchases.vercel.app"
+                        //"http://localhost:5173" // In case testing FE locally
+                        )
+                    .AllowCredentials();
 
-                    policy.AllowAnyHeader();
-                    policy.AllowAnyMethod();
-                });
+                policy.AllowAnyHeader();
+                policy.AllowAnyMethod();
             });
+        });
 
-            // Add services to the container.
-            builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+        // Add services to the container.
+        builder.Services.AddControllers();
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
 
-            // Dependency Injection Setup for Services
-            builder.Services.AddScoped<IPurchaseService, PurchaseService>();
-            builder.Services.AddScoped<IVendorService, VendorService>();
-            builder.Services.AddScoped<IReceiptRetrieverService, ReceiptRetrieverService>();
-            builder.Services.AddScoped<IMessageNotifier, TelegramMessageNotifier>();
-            builder.Services.AddScoped<IRemoteFileManager, TelegramRemoteFileManager>();
-            builder.Services.AddScoped<ILlmProcessor, LlmProcessor>();
+        // Dependency Injection Setup for Services
+        builder.Services.AddScoped<IPurchaseService, PurchaseService>();
+        builder.Services.AddScoped<IVendorService, VendorService>();
+        builder.Services.AddScoped<IReceiptRetrieverService, ReceiptRetrieverService>();
+        builder.Services.AddScoped<IMessageNotifier, TelegramMessageNotifier>();
+        builder.Services.AddScoped<IRemoteFileManager, TelegramRemoteFileManager>();
+        builder.Services.AddScoped<ILlmProcessor, LlmProcessor>();
 
-            // DI for Repos
-            builder.Services.AddScoped<IPurchaseRepository, PurchaseRepository>();
-            builder.Services.AddScoped<IVendorRepository, VendorRepository>();
+        // DI for Repos
+        builder.Services.AddScoped<IPurchaseRepository, PurchaseRepository>();
+        builder.Services.AddScoped<IVendorRepository, VendorRepository>();
 
-            builder.Services.Configure<MongoDbSettings>(builder.Configuration.GetSection("PurchasesDatabase"));
-            builder.Services.Configure<TelegramIntegrationSettings>(builder.Configuration.GetSection("TelegramIntegration"));
-            builder.Services.Configure<OpenAiSettings>(builder.Configuration.GetSection("OpenAiIntegration"));
+        builder.Services.Configure<MongoDbSettings>(builder.Configuration.GetSection("PurchasesDatabase"));
+        builder.Services.Configure<TelegramIntegrationSettings>(builder.Configuration.GetSection("TelegramIntegration"));
+        builder.Services.Configure<OpenAiSettings>(builder.Configuration.GetSection("OpenAiIntegration"));
 
-            builder.Services.AddSingleton<IMongoClient>(sp =>
-            {
-                var mongoDbSection = builder.Configuration.GetSection("PurchasesDatabase");
-                var connString = mongoDbSection.GetValue<string>("ConnectionString") ?? throw new InvalidOperationException("Unable to retrieve Mongo db settings");
+        builder.Services.AddSingleton<IMongoClient>(sp =>
+        {
+            var mongoDbSection = builder.Configuration.GetSection("PurchasesDatabase");
+            var connString = mongoDbSection.GetValue<string>("ConnectionString") ?? throw new InvalidOperationException("Unable to retrieve Mongo db settings");
 
-                return new MongoClient(connString);
-            });
+            return new MongoClient(connString);
+        });
 
-            // Generic Scraper Client Setup
-            builder.Services.AddHttpClient("Scraper", client =>
-            {
-                client.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36");
-                client.Timeout = TimeSpan.FromSeconds(20);
-            });
+        // Generic Scraper Client Setup
+        builder.Services.AddHttpClient("Scraper", client =>
+        {
+            client.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36");
+            client.Timeout = TimeSpan.FromSeconds(20);
+        });
 
-            var app = builder.Build();
+        var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
             app.UseSwagger();
             app.UseSwaggerUI();
-
-            app.UseHttpsRedirection();
-            app.MapControllers();
-            app.UseRouting();
-            app.UseCors(allowReactAppCorsName);
-            app.UseAuthorization();
-
-            app.Run();
         }
+
+        app.UseHttpsRedirection();
+        app.MapControllers();
+        app.UseRouting();
+        app.UseCors(allowReactAppCorsName);
+        app.UseAuthorization();
+
+        app.Run();
     }
 }
